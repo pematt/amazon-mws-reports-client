f0e5985eac5e459ec15c5a97ad92393daf5e6418
Mattias Glaving <mattias@sellersolve.com>
2019-05-24 20:34:06 +0100

    amended defaults

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec); // https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
 const path = require('path');
 
-// TODO: comment out the parametersExample
-const parametersExample = {
+const parametersDefaults = {
   connection: {
     MaxErrorRetry: 3, // mandatory
     ProxyHost: null,   // null if no proxy // mandatory
@@ -11,44 +10,43 @@ const parametersExample = {
     MwsAuthToken: null // Optional
   },
   target: {
-    MwsServiceURL: 'url', // mandatory
-    MwsMarketplaceId: 'aws marketplace id' // mandatory
+    MwsServiceURL: null, // mandatory
+    MwsMarketplaceId: null // mandatory
   },
   developer: {
-    ApplicationName: 'application name', // mandatory
-    ApplicationVersion: 'application version' // mandatory
+    ApplicationName: null, // mandatory
+    ApplicationVersion: null // mandatory
   },
   merchant: {
-    AwsMerchantId: 'aws merchant id', // mandatory
-    MwsAccessKeyId: 'aws access key id', // mandatory
-    MwsSecretAccessKey: 'aws secret access key' // mandatory
+    AwsMerchantId: null, // mandatory
+    MwsAccessKeyId: null, // mandatory
+    MwsSecretAccessKey: null // mandatory
   },
   query: {
-    MwsReportType: '_GET_MERCHANT_LISTINGS_DATA_', // mandatory
-    ReportStart: 'timestamp', // mandatory
-    ReportEnd: 'timestamp', // mandatory
+    MwsReportType: null, // mandatory
+    ReportStart: null, // mandatory
+    ReportEnd: null, // mandatory
     ReportOptions: null // further report options, optional, eg 'ShowSalesChannel=true'
   },
   result: { // where to store the data fetched from mws
-    destination: 'file', // file or stdout, optional
+    destination: 'stdout', // file or stdout, optional
     file: { // manadory if destination = 'file', otherwise ignored
       path: '$MWS_EXTRACT_DIR', // directory where to store the file
       name: 'file.xml', // filename. appended to path
-      compression: null, // if destination 'file'; 'gzip' = gzip the file, https://nodejs.org/api/zlib.html
-      namePostfix: null // any filename postfix; '.gzip' = file.xml becomes file.xml.gzip
+      compression: 'gzip', // if destination 'file'; 'gzip' = gzip the file, https://nodejs.org/api/zlib.html
+      namePostfix: '.gzip' // any filename postfix; '.gzip' = file.xml becomes file.xml.gzip
     }
   },
   exec: {  // optional
     encoding: 'utf8',
     env: {
-      KEY1: 'value1',
-      KEY2: 'value2'
+      MWS_EXTRACT_DIR: '.'
     },
     timeout: 30 * 1000, // ms
     maxBuffer: 1024 * 1024 // bytes
   },
   system: {
-    phpCommand: 'php' // in case there are more than one way to invoke php then one can select one here, optional
+    phpCommand: 'php' // optional
   },
   dev: { // optional
     debug: false,
@@ -63,34 +61,24 @@ function ClientException(message) {
    this.npmPacket = 'amazon-mws-reports-client';
 }
 
-module.exports = async function (parameters) {
+module.exports = async function (parametersIn) {
 
-  if (!(parameters)) {
+  if (!(parametersIn)) {
     throw new ClientException('No parameters received');
   }
-  if (!(parameters.merchant)) {
+  if (!(parametersIn.merchant)) {
     throw new ClientException('Parameters missing mandatory property "merchant"');
   }
+
+  // parametersIn's properties overwrites parametersDefault's properties with the same name
+  const parameters = { ...parametersDefaults, ...parametersIn }
 
   const exec = util.promisify(require('child_process').exec);
   const phpScript = path.dirname(__filename) + "/php/RequestReport.php";
   const loc = 'amazon-mws-reports-client:';
   const debug = parameters.dev && parameters.dev.debug;
-  const execDefaults = {
-    encoding: 'utf8',
-    maxBuffer: 1024 * 2048,
-    timeout: 60 * 1000 // ms
-  }
-  const systemDefaults = {
-    phpCommand: 'php'
-  }
 
-  // object spread
-  // later properties overwrite earlier properties with the same name
-  const execOptions = {...execDefaults, ...parameters.exec }
-  const systemOptions = {...systemDefaults, ...parameters.system }
-
-  const command = systemOptions.phpCommand + " " + phpScript + " '" + JSON.stringify(parameters) + "'"
+  const command = parameters.system.phpCommand + " " + phpScript + " '" + JSON.stringify(parameters) + "'"
   // + " | grep 'END:-_]%£j+:'"
   // + " | tail -n 1" // return only the last line
   ;
@@ -98,14 +86,12 @@ module.exports = async function (parameters) {
   if (debug) {
     console.log(loc, 'phpScript:', phpScript);
     console.log(loc, 'parameters:', parameters);
-    console.log(loc, 'execOptions:', execOptions);
-    console.log(loc, 'systemOptions:', systemOptions);
     console.log(loc, 'command:', command);
     console.log(loc, 'path.dirname:', path.dirname(__filename));
   }
 
   try {
-    const { stdout, stderr } = await exec(command, execOptions);
+    const { stdout, stderr } = await exec(command, parameters.exec);
     if (stderr) {
       console.error(loc, `error: ${stderr}`);
     }
