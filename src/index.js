286e4081548ba58d92fdcb972770d284e8fc3363
Mattias Glaving <mattias@sellersolve.com>
2019-06-06 23:00:06 +0100

    installed deepmerge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+'use strict';
+
 const util = require('util');
 const exec = util.promisify(require('child_process').exec); // https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
 const path = require('path');
+const merge = require('deepmerge');
 
 const PACKET_NAME = 'amazon-mws-reports-client';
 const PACKET_NAME_HR = 'Amazon MWS Reports Client Exception';
@@ -9,7 +12,7 @@ const ERR_TYPE_MISSING = 1;
 
 // https://mws.amazonservices.com/scratchpad/index.html
 
-const parametersDefault = {
+const argsDefault = {
   api: { //* connection
     //countryCode: null, // mandatory [AE,AU,BR,CA,CN,DE,ES,FR,GB,IN,IT,JP,MX,NL,TR,US]
     mwsSection: null, // Mandatory [EasyShip,Feeds,Finances,FulfillmentInboundShipment,FulfillmentInventory,FulfillmentOutboundShipping,MerchantFulfillment,OffAmazonPayments,Orders,Products,Recommendations,Reports,Sellers,Subscriptions]
@@ -42,6 +45,7 @@ const parametersDefault = {
     stdout: { // optional, used only if destination = 'stdout'
       dataLinePrefix: '[Data Line]:\t', // prefix for all lines of data downloaded from mws
       format: 'json',
+      maxLines: 10000, // max number of lines to return
     },
     file: { // manadory if destination = 'file', otherwise ignored
       path: '$MWS_EXTRACT_DIR', // directory where to store the file
@@ -70,7 +74,7 @@ const parametersDefault = {
   }
 }
 
-mwsApiOperationParametersDefault = {
+mwsApiOperationArgsDefault = {
   requestReport: {
     required: {
       reportType: null,
@@ -93,51 +97,57 @@ function ClientException(message, type = ERR_TYPE_QUOTE) {
 
   switch (type) {
     case ERR_TYPE_MISSING:
-      this.message = 'The input parameters object is missing the mandatory property "' + message + '".';
+      this.message = 'The input arguments object is missing the mandatory property "' + message + '".';
       break;
     default:
       this.message = message;
    }
 }
 
-module.exports = async function (parametersIn) {
+module.exports = async function (args) {
 
-  if (!(parametersIn)) {
-    throw new ClientException('No parameters received.');
+  if (!(args)) {
+    throw new ClientException('No arguments received.');
   }
-  if (!(parametersIn.api.merchant)) {
+  if (!(args.api.merchant)) {
     throw new ClientException('merchant', ERR_TYPE_MISSING);
   }
   // TODO: check all mandatory properties here
 
   // parametersIn's properties overwrites parametersDefault's properties with the same name
-  const parameters = { ...parametersDefault, ...parametersIn }
+  // todo: proper deep copy, not just 1st level... lodash?
+  const phpArgs = { ...argsDefault, ...args }
 
   const exec = util.promisify(require('child_process').exec);
   const phpScript = path.dirname(__filename) + "/php/RequestReport.php";
-  const debug = parameters.dev && parameters.dev.debug;
+  const debug = phpArgs.dev && phpArgs.dev.debug;
 
-  const command = parameters.system.phpCommand + " " + phpScript + " '" + JSON.stringify(parameters) + "'"
-  // + " | grep 'END:-_]%£j+:'"
-  // + " | tail -n 1" // return only the last line
+  const phpCommand =
+    phpArgs.system.phpCommand + " " + phpScript + " '" + JSON.stringify(phpArgs) + "'"
+    // + " | grep 'END:-_]%£j+:'"
+    + " | tail -n " + phpArgs.result.stout.maxLines; // return only the last maxLines lines
   ;
 
   if (debug) {
     console.log(PACKET_NAME, ': phpScript:', phpScript);
-    console.log(PACKET_NAME, ': parameters:', parameters);
-    console.log(PACKET_NAME, ': command:', command);
+    console.log(PACKET_NAME, ': phpArgs:', phpArgs);
+    console.log(PACKET_NAME, ': phpCommand:', phpCommand);
     console.log(PACKET_NAME, ': path.dirname:', path.dirname(__filename));
   }
 
   try {
-    const { stdout, stderr } = await exec(command, parameters.exec);
+    const { stdout, stderr } = await exec(phpCommand, phpArgs.exec);
     if (stderr) {
       console.error(PACKET_NAME, `: error: ${stderr}`);
     }
     if (debug) {
       console.log(PACKET_NAME, `: Response: ${stdout}`);
     }
-    return stdout;
+    // return the output of the phpCommand
+    return {
+      stdout: stdout,
+      stderr: stderr
+    };
   }
   catch (err) {
     console.error(PACKET_NAME, err);
